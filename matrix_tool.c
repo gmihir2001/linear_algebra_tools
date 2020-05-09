1553e017ffe5a33faa6696dfdbd2c0f6aedd3cb0
gmihir2001 <mihirgupta2019@gmail.com>
2020-05-09 17:39:14 -0400

    Implement getting matrix from user input

--- matrix_tool.c
+++ matrix_tool.c
@@ -8,10 +8,28 @@ int main(void) {
 	struct matrix mat_01;
 	struct matrix * mat_01_ptr = & mat_01;
 
-	int arr[6] = {1, 2, 3, 4, 5, 6};
+	// get the info for the matrix.
+	printf("Please enter the dimensions of the matrix: ");
 
-	matrix_init(mat_01_ptr, 2, 3, arr);
+	int rows = 0;
+	int cols = 0;
+
+	scanf("%d %d", & rows, & cols);
+
+	printf("Please enter the matrix itself (flat form): ");
+
+	// for now, just use an array that has 64 spots...
+	// should be able to hold the data.
+
+	int input_arr[64];
+
+	for(int i = 0; i < (rows * cols); i++) {
+		scanf("%d", & input_arr[i]);
+	}
+
+	matrix_init(mat_01_ptr, rows, cols, input_arr);
 
 	matrix_print(mat_01_ptr);
+
 	return 0;
 }
