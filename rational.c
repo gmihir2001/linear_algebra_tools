e14a07483862f33a49e0c4937c553569faa9331d
gmihir2001 <mihirgupta2019@gmail.com>
2020-05-17 11:50:45 -0400

    UNFINISHED COMMIT - rational implementation

--- rational.c
+++ rational.c
@@ -2,6 +2,30 @@
 
 #include "rational.h"
 
+// return the gcd of the two number.
+int gcd(int a, int b)
+{
+	if (a == 0 || b == 1) {
+		return 1;
+	}
+
+	if (a > b){
+		int temp;
+		temp = b;
+		b = a;
+		a = temp;
+	}
+	 int remainder;
+	 remainder = a%b;
+
+	 while (remainder !=0){
+		a = b;
+		b = remainder;
+		remainder = a % b;
+	 }
+	return b;
+}
+
 struct rational rational_create(int num, int den) {
 	struct rational ret;
 	ret.num = num;
@@ -13,9 +37,48 @@ struct rational rational_create(int num, int den) {
 struct rational rational_add(struct rational a, struct rational b) {
 	struct rational result;
 
-	result.num = a.num + b.num;
-	result.den = a.den;
+	result.num = (a.num * b.den) + (b.num * a.den);
 
+	result.den = a.den * b.den;
+
+	int rational_gcd = gcd(result.num, result.den);
+
+	result.num = result.num / rational_gcd;
+	result.den = result.den / rational_gcd;
+
+	return result;
+}
+
+struct rational rational_mult(struct rational a, struct rational b) {
+	struct rational result;
+	result.num = (a.num * b.num);
+	result.den = (a.den * b.den);
+	int rational_gcd = gcd(result.num, result.den);
+	result.num = result.num / rational_gcd;
+	result.den = result.den / rational_gcd;
+	return result;
+}
+
+struct rational rational_subt(struct rational a, struct rational b) {
+
+	struct rational result;
+	result.num = (a.num * b.den) - (b.num * a.den);
+	result.den = a.den * b.den;
+	int rational_gcd = gcd(result.num, result.den);
+	result.num = result.num / rational_gcd;
+	result.den = result.den / rational_gcd;
+	return result;
+}
+
+struct rational rational_div(struct rational a, struct rational b) {
+	struct rational result;
+	result.num = (a.num * b.den);
+	result.den = (a.den * b.num);
+
+	int rational_gcd = gcd(result.num, result.den);
+	printf("Got the gcd as %d\n", rational_gcd);
+	result.num = result.num / rational_gcd;
+	result.den = result.den / rational_gcd;
 	return result;
 }
 
@@ -24,5 +87,9 @@ bool rational_eq(struct rational a, struct rational b) {
 }
 
 void rational_print(struct rational a) {
-	printf("%d/%d", a.num, a.den);
+	if ( a.den == 1 ) {
+		printf("%d\t", a.num);
+	} else {
+		printf("%d/%d\t", a.num, a.den);
+	}
 }
